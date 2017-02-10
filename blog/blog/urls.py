a6eefb13502afab4824953f255bfd4a5ea6c027f
jaychia <jaychia94@gmail.com>
2017-02-10 16:21:48 -0500

    initial setup done

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -8,6 +8,7 @@ from django.views.i18n import set_language
 from mezzanine.core.views import direct_to_template
 from mezzanine.conf import settings
 import mezzanine.blog.views
+import blogtemplate.views as blog_views
 
 
 admin.autodiscover()
@@ -67,6 +68,9 @@ urlpatterns += [
 
     url("^$", mezzanine.blog.views.blog_post_list, name="home"),
 
+    #OWN_URLS
+    url("banner", blog_views.scrolling_banner, name="scrolling_banner"),
+
     # MEZZANINE'S URLS
     # ----------------
     # ADD YOUR OWN URLPATTERNS *ABOVE* THE LINE BELOW.
@@ -78,7 +82,7 @@ urlpatterns += [
     # ``mezzanine.urls``, go right ahead and take the parts you want
     # from it, and use them directly below instead of using
     # ``mezzanine.urls``.
-    url("^", include("mezzanine.urls")),
+    url(r"^", include("mezzanine.urls")),
 
     # MOUNTING MEZZANINE UNDER A PREFIX
     # ---------------------------------
--- blog/blogtemplate/views.py
+++ blog/blogtemplate/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
+from mezzanine.blog.models import BlogPost
+
+def scrolling_banner(request):
+	posts = BlogPost.objects.all()
+	current_site = request.META['HTTP_HOST']
+	ctd = {
+		"posts": posts,
+		"current_site": current_site,
+	}
+	return render(request, 'generic/includes/banner.html', ctd)
